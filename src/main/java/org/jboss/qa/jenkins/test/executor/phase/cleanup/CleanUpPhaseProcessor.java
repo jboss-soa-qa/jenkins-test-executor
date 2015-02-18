af5652977962ba102f1a624f49f3a21a1585d314
Vaclav Chalupa <vchalupa@redhat.com>
2015-02-18 17:54:56 +0100

    Make workspace cleanup optional

--- src/main/java/org/jboss/qa/jenkins/test/executor/phase/cleanup/CleanUpPhaseProcessor.java
+++ src/main/java/org/jboss/qa/jenkins/test/executor/phase/cleanup/CleanUpPhaseProcessor.java
@@ -18,12 +18,13 @@ public class CleanUpPhaseProcessor extends PhaseDefinitionProcessor {
 
 	public void execute() {
 		log.debug("@{} - {}", CleanUp.class.getName(), cleanUp.id());
-		try {
-			// TODO(vchalupa): Make a decision about this, some parameter in annotation may be set...
-			// TODO(vchalupa): Without clean the unpacked server home can not be recognized
-			FileUtils.deleteDirectory(JenkinsTestExecutor.WORKSPACE);
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		if (cleanUp.cleanWorkspace()) {
+			try {
+				FileUtils.deleteDirectory(JenkinsTestExecutor.WORKSPACE);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }

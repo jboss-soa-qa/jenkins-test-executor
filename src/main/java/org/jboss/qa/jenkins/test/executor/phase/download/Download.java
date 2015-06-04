c31ee2f135ce594bb8f8f924c5c4178225e3dca9
Martin Basovnik <mbasovni@redhat.com>
2015-06-04 11:26:35 +0200

    Issue #25 Make job phases inheritable
    - Upgrade version of phaser

--- src/main/java/org/jboss/qa/jenkins/test/executor/phase/download/Download.java
+++ src/main/java/org/jboss/qa/jenkins/test/executor/phase/download/Download.java
@@ -19,10 +19,12 @@ import org.jboss.qa.phaser.Id;
 import org.jboss.qa.phaser.Order;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Download {
